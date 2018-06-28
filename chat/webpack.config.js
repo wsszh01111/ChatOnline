8b6dbea639d16090e7d14741a0e6ed3a11656609
CheJiData <eton.kweong@jikelink.cn>
2018-06-28 18:01:00 +0800

    start using express as server

--- chat/webpack.config.js
+++ chat/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry:path.resolve(__dirname, 'src/index.js'),
     output:{
-        path:path.resolve(__dirname, 'public'),
+        path:path.resolve(__dirname, '../server/public'),
         filename:'[name].bundle.js'
     },
     module: {
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
