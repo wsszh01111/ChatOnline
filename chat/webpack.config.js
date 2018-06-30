c1a6549ff574380cb3d2300a3be11e844eba2f24
wsszh01111 <wsszh01111@163.com>
2018-06-30 17:09:59 +0800

    add style-loader and css-loader to load css file

--- chat/webpack.config.js
+++ chat/webpack.config.js
@@ -8,11 +8,13 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
+            test:/\.js$/,
+            exclude:/node_modules/,
+            use:'babel-loader'
+        },{
+            test:/\.css$/,
+            exclude:/node_modules/,
+            use:['style-loader', 'css-loader']
         }]
     }
 }
