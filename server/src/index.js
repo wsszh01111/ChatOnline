9d0cb5be8e243c7caba44f31be0db62bd92ddb98
wsszh01111 <wsszh01111@163.com>
2018-07-05 01:05:25 +0800

    reuse express as development server, add auto reflesh with supervisor and reload

--- server/src/index.js
+++ server/src/index.js
@@ -5,21 +5,11 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('public'));
+app.use(express.static('reload'));
 
 app.get('/', function(req, res){
     console.log('url---', req.url)
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
-});
-
-// default events are connect/disconnect
-io.on('connect', function(socket){
-    socket.emit('connected', socket.id);
-    console.log('a user connected, whose id is ', socket.id);
-    socket.on('message', function(msg){
-        console.log('user whose id is ', socket.id, ' sent a message: ', msg);
-        io.emit('message', {id:socket.id, msg});
-        console.log('send all people: ', msg);
-    })
 });
 
 http.listen(80, function(){
